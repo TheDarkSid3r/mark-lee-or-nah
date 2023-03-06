e5970650261145b39b9fea014f087e49b851465a
TheDarkSid3r <sid3rdark@gmail.com>
2023-03-05 20:03:51 -0800

    autoplay

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@ import { marklee } from "./marklee";
 const sizes = [[240, 360], [240, 160], [281, 360], [246, 360], [252, 360], [236, 360], [268, 360], [294, 360], [238, 240], [240, 234], [240, 235], [240, 240], [235, 360], [239, 360], [257, 360], [241, 360], [291, 360], [240, 680], [288, 360], [253, 360], [233, 360], [231, 360], [248, 360], [960, 720], [640, 960], [639, 960], [720, 960], [960, 640], [1366, 1372], [1344, 2015], [1365, 1356], [1343, 2015], [1440, 960], [1345, 2015], [641, 960], [1333, 1344], [960, 641]];
 
 const minLoadingTime = 1000;
+const sequence = ["up", "up", "down", "down", "left", "right", "left", "right", "b", "a", "start"];
 
 export default defineComponent({
     data(): {
@@ -17,6 +18,8 @@ export default defineComponent({
         wasCorrect: boolean;
         score: number;
         highScore: number;
+        autoplay: boolean;
+        sequenceNum: number;
     } {
         const highScore = localStorage.getItem("mlhighscore");
         return {
@@ -27,7 +30,9 @@ export default defineComponent({
             hasResult: false,
             wasCorrect: false,
             score: 0,
-            highScore: !highScore || isNaN(highScore as any) ? 0 : parseInt(highScore!)
+            highScore: !highScore || isNaN(highScore as any) ? 0 : parseInt(highScore!),
+            autoplay: false,
+            sequenceNum: 0
         };
     },
     methods: {
@@ -49,6 +54,7 @@ export default defineComponent({
             window.setTimeout(() => {
                 this.isLoading = false;
                 this.drawImage();
+                if (this.autoplay) window.setTimeout(this.answerCorrectly, 150);
             }, Math.max(0, minLoadingTime - (Date.now() - this.loadingStart)));
         },
         drawImage() {
@@ -63,6 +69,10 @@ export default defineComponent({
             ctx.clearRect(0, 0, newWidth, newHeight);
             ctx.drawImage(this.image, 0, 0, newWidth, newHeight);
         },
+        choose(markLee: boolean) {
+            if (this.autoplay) return;
+            this.answer(markLee);
+        },
         answer(markLee: boolean) {
             if (this.isLoading) return;
             this.wasCorrect = markLee === this.isMarkLee;
@@ -76,6 +86,37 @@ export default defineComponent({
             }
             this.hasResult = true;
             this.setup();
+        },
+        answerCorrectly() {
+            this.answer(this.isMarkLee);
+        },
+        startAutoplay() {
+            this.autoplay = true;
+            this.answerCorrectly();
+        },
+        keypress(e: KeyboardEvent) {
+            if (this.autoplay) return;
+
+            const key = this.getSequenceKey(e.key);
+            if (!key) return;
+            if (key === sequence[this.sequenceNum]) {
+                this.sequenceNum++;
+                if (this.sequenceNum >= sequence.length) this.startAutoplay();
+            } else this.sequenceNum = 0;
+        },
+        getSequenceKey(key: string) {
+            switch (key) {
+                case "ArrowUp": return "up";
+                case "ArrowDown": return "down";
+                case "ArrowLeft": return "left";
+                case "ArrowRight": return "right";
+                case "a":
+                case "A": return "a";
+                case "b":
+                case "B": return "b";
+                case "Enter":
+                case " ": return "start";
+            }
         }
     },
     computed: {
@@ -86,6 +127,10 @@ export default defineComponent({
     mounted() {
         this.setup();
         this.image.addEventListener("load", this.loaded);
+        window.addEventListener("keyup", this.keypress);
+    },
+    beforeUnmount() {
+        window.removeEventListener("keyup", this.keypress);
     },
     components: { ChoiceButton }
 });
@@ -111,9 +156,9 @@ export default defineComponent({
             </div>
         </div>
         <div class="choices" :class="{ disabled: isLoading }">
-            <ChoiceButton @click="answer(true)">MARK LEE</ChoiceButton>
+            <ChoiceButton @click="choose(true)">MARK LEE</ChoiceButton>
             <div class="or">OR</div>
-            <ChoiceButton @click="answer(false)">NAH</ChoiceButton>
+            <ChoiceButton @click="choose(false)">NAH</ChoiceButton>
         </div>
     </div>
 </template>
